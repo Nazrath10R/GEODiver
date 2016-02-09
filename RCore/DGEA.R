9f4f0f0177580c1226af8b567a82d0b7d655f670
IsmailM <ismail.moghul@gmail.com>
2016-02-09 20:29:28 +0000

    update RCore

--- RCore/DGEA.R
+++ RCore/DGEA.R
@@ -3,7 +3,7 @@
 # Filename      : DGEA.R                                   #
 # Authors       : IsmailM, Nazrath, Suresh, Marian, Anisa  #
 # Description   : Differential Gene Expression Analysis    #
-# Rscript DGEA.R --accession GDS5093 --factor "disease.state" --popA "Dengue Hemorrhagic Fever,Convalescent,Dengue Fever" --popB "healthy control" --popname1 "Dengue" --popname2 "Normal" --topgenecount 250 --foldchange 0.3 --thresholdvalue 0.005 --distance "euclidean" --clustering "average" --dbrdata ~/Desktop/GDS5093.rData --outputdir ~/Desktop/ --heatmaprows 100 --dendrow TRUE --dendcol TRUE --analyse "Boxplot,Volcano,PCA,Heatmap" --expsavepath ~/Desktop/topexpr.rData
+# Rscript DGEA.R --accession GDS5093 --factor "disease.state" --popA "Dengue Hemorrhagic Fever,Convalescent,Dengue Fever" --popB "healthy control" --popname1 "Dengue" --popname2 "Normal" --topgenecount 250 --foldchange 0.3 --thresholdvalue 0.005 --distance "euclidean" --clustering "average" --dbrdata ~/Desktop/GDS5093.rData --rundir ~/Desktop/ --heatmaprows 100 --dendrow TRUE --dendcol TRUE --analyse "Boxplot,Volcano,PCA,Heatmap,Clustering" --adjmethod fdr
 # ---------------------------------------------------------#
 
 #############################################################################
@@ -17,10 +17,13 @@ suppressMessages(library("GEOquery"))
 suppressMessages(library("pheatmap"))
 suppressMessages(library("plyr"))
 suppressMessages(library("DMwR"))
+suppressMessages(library("dendextend"))
+suppressMessages(library("squash"))
 
 # load required libraries
 library("argparser")    # Argument passing
 library("Cairo")        # Plots saving
+library("dendextend")   # Dendogram extended functionalities
 library("DMwR")         # Outlier Prediction for clustering
 library("GEOquery")     # GEO dataset Retrieval
 library("ggplot2")      # Graphs designing
@@ -31,6 +34,7 @@ library("limma")        # Differencial Gene Expression Analysis
 library("plyr")         # Splitting, Applying and Combining Data
 library("RColorBrewer") # Import Colour Pallete
 library("reshape2")     # Prepare dataset for ggplot
+library("squash")       # Clustering Dendogram
 
 
 #############################################################################
@@ -41,7 +45,7 @@ library("reshape2")     # Prepare dataset for ggplot
 parser <- arg_parser("This parser contains the input arguments")
 
 # General Parameters
-parser <- add_argument(parser, "--outputdir",
+parser <- add_argument(parser, "--rundir",
     help = "The outout directory where graphs get saved")
 parser <- add_argument(parser, "--dbrdata",
     help = "Downloaded GEO dataset full path")
@@ -49,8 +53,6 @@ parser <- add_argument(parser, "--analyse",
     help = "List of analysis to be performed", nargs = "+")
 parser <- add_argument(parser, "--geodbpath",
     help  =  "GEO Dataset full path")
-parser <- add_argument(parser, "--expsavepath",
-    help  =  "Toptable Expression data saving full path")
 
 # Sample Parameters
 parser <- add_argument(parser, "--accession",
@@ -66,11 +68,15 @@ parser <- add_argument(parser, "--popname1",
 parser <- add_argument(parser, "--popname2",
     help = "name for GroupB")
 
+# Toptable
+parser <- add_argument(parser, "--topgenecount",
+                       help = "Number of top genes to be used")
+parser <- add_argument(parser, "--adjmethod",
+                       help = "Adjust P-values for Multiple Comparisons")
+
 # Volcano plot Parameters
 parser <- add_argument(parser, "--foldchange",
     help = "fold change cut off")
-parser <- add_argument(parser, "--topgenecount",
-    help = "number of top genes to be used")
 parser <- add_argument(parser, "--thresholdvalue",
     help = "threshold value cut off")
 
@@ -94,7 +100,7 @@ argv   <- parse_args(parser)
 #############################################################################
 
 # General Parameters
-output.dir      <- argv$outputdir
+run.dir      <- argv$rundir
 dbrdata         <- argv$dbrdata
 analysis.list   <- unlist(strsplit(argv$analyse, ","))
 
@@ -107,11 +113,20 @@ pop.name2       <- argv$popname2
 pop.colour1     <- "#b71c1c"  # Red
 pop.colour2     <- "#0d47a1"  # Blue
 
-# Volcano plot Parameters
+# Toptable
 topgene.count   <- as.numeric(argv$topgenecount)
+toptable.sortby <- "p"
+if (argv$adjmethod %in%
+    c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none")){
+    adj.method      <- argv$adjmethod
+}else{
+    adj.method      <- "fdr"
+}
+
+# Volcano plot Parameters
 fold.change     <- as.numeric(argv$foldchange)
 threshold.value <- as.numeric(argv$thresholdvalue)
-toptable.sortby <- "p"
+
 
 # Clustering
 if (argv$distance %in%
@@ -134,11 +149,9 @@ heatmap.rows <- as.numeric(argv$heatmaprows)
 dendrow <- as.logical(argv$dendrow)
 dendcol <- as.logical(argv$dendcol)
 
-
 #############################################################################
 #                        Load GEO Dataset to Start Analysis                 #
 #############################################################################
-
 
 if (file.exists(dbrdata)){
     load(file = dbrdata)
@@ -156,17 +169,17 @@ if (file.exists(dbrdata)){
 
 X <- exprs(eset)  # Get Expression Data
 
-# auto-detect if data needs transformation and log2 transform if needed
+# auto-detect if data is log transformed 
 qx <- as.numeric(quantile(X, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm = T))
 logc <- (qx[5] > 100) ||
     (qx[6] - qx[1] > 50 && qx[2] > 0) ||
     (qx[2] > 0 && qx[2] < 1 && qx[4] > 1 && qx[4] < 2)
 
-if (logc) {
+# If not log transformed, do the log2 transformed
+if (logc == TRUE) {
     X[which(X <= 0)] <- NaN
     exprs(eset) <- log2(X)
 }
-
 
 #############################################################################
 #                       Factor Selection                                    #
@@ -237,11 +250,11 @@ find.toptable <- function(X, newpclass, toptable.sortby, topgene.count){
     fit <- contrasts.fit(fit, contrasts)
 
     # empirical Bayes smoothing to standard errors
-    fit <- eBayes(fit)
+    fit <- eBayes(fit, proportion = 0.01)
 
     # Create top Table
-    toptable <- topTable(fit,
-                         sort.by = toptable.sortby, # 'p' or 'LogFC'
+    toptable <- topTable(fit,adjust.method = adj.method,
+                         sort.by = toptable.sortby, 
                          number = topgene.count)
 
     return(toptable)
@@ -253,9 +266,28 @@ find.toptable <- function(X, newpclass, toptable.sortby, topgene.count){
 
 # Boxplot
 samples.boxplot <- function(data, pop.colours, pop.names, path){
-    boxplot <- ggplot(data) + geom_boxplot(aes(x = Var2, y = value, colour = Groups)) + theme(axis.text.x = element_text(angle = 70, hjust = 1), legend.position = "right")+ labs(x = "Samples", y = "Expression Levels") + scale_color_manual(name = "Groups", values = pop.colours, labels = pop.names)
+    boxplot <- ggplot(data) + geom_boxplot(aes(x = Var2, y = value, colour = Groups), outlier.shape = NA) + theme(axis.text.x = element_text(angle = 70, hjust = 1), legend.position = "right")+ labs(x = "Samples", y = "Expression Levels") + scale_color_manual(name = "Groups", values = pop.colours, labels = pop.names)
+    # compute lower and upper whiskers
+    ylim1 = boxplot.stats(data$value)$stats[c(1, 5)]
+    
+    # scale y limits based on ylim1
+    boxplot <- boxplot + coord_cartesian(ylim = ylim1*1.05)
+    
     filename <- paste(path, "boxplot.png", sep = "")
     ggsave(filename, plot = boxplot, width = 8, height = 4)
+}
+
+# Calculate Outliers Probabilities/ Dissimilarities
+outlier.probability <- function(X, dist.method = "euclidean", clust.method = "average"){
+    # Rank outliers using distance and clustering parameters
+    o <- outliers.ranking(t(X),
+                          test.data   = NULL,
+                          method      = "sizeDiff", # Outlier finding method
+                          method.pars = NULL,
+                          clus = list(dist = dist.method,
+                                      alg  = "hclust",
+                                      meth = clust.method))
+    return(o$prob.outliers)
 }
 
 # Heatmap
@@ -271,18 +303,11 @@ heatmap <- function(X.matix, exp, heatmap.rows = 100, dendogram.row, dendogram.c
                     method = dist.method),
                     method = clust.method)
 
-        # Rank outliers using distance and clustering parameters
-        o <- outliers.ranking(t(X.matix),
-                              test.data   = NULL,
-                              method      = "sizeDiff", # Outlier finding method
-                              method.pars = NULL,
-                              clus = list(dist = dist.method,
-                                          alg  = "hclust",
-                                          meth = clust.method))
+        outliers <- outlier.probability(X.matix, dist.method, clust.method)
 
         ann.col <- data.frame(  Population = exp[, "population"],
                                 Factor     = exp[, "factor.type"],
-                                Outliers   = o$prob.outliers)
+                                Dissimilarity   = outliers)
         column.gap <- 0
     }else{
         hc <- FALSE
@@ -302,6 +327,7 @@ heatmap <- function(X.matix, exp, heatmap.rows = 100, dendogram.row, dendogram.c
              cluster_row    = dendogram.row,
              cluster_cols   = hc,
              annotation_col = ann.col,
+             legend         = TRUE,
              color          = col.pal,
              fontsize       = 6.5,
              fontsize_row   = 3.5,
@@ -310,23 +336,72 @@ heatmap <- function(X.matix, exp, heatmap.rows = 100, dendogram.row, dendogram.c
     dev.off()
 }
 
+#Clustering dendogram
+clustering <- function(X, dist.method = "euclidean", clust.method = "average", exp){
+    
+    dendo  <-  hclust(dist(t(X), method = dist.method), method = clust.method) 
+    
+    # Factor types
+    factor <- as.factor(exp[,"factor.type"])
+    names(factor) <- exp$Sample
+    population <- as.factor(exp[,"population.colour"])
+    names(population ) <- exp$Sample
+    outliers <- outlier.probability(X, dist.method, clust.method)
+    
+    factor.cmap <- makecmap(as.numeric(factor), n = length(levels(factor)), colFn = colorRampPalette(c('black', 'green')))
+    population.cmap <- makecmap(as.numeric(factor), n = length(levels(population)), colFn = colorRampPalette(c('black', 'blue')))
+    outliers.cmap <- makecmap(outliers, n = 10, colFn = colorRampPalette(c('black', 'red')))
+    
+    matrix <- data.frame(Factor =  factor, 
+                         Groups = population, 
+                         Dissimilarity = cmap( outliers, outliers.cmap))
+    jColors <-
+        with(matrix,
+             data.frame(factors = levels(Factor),
+                        color = I(brewer.pal(nlevels(Factor), name = 'Dark2'))))
+    
+    matrix <- within(matrix,{
+        Factor = jColors$color[matrix$Factor]
+    })
+    
+        filename <- paste(run.dir,"clustering.png",sep = "")
+        CairoPNG(file = filename, width = 1200, height = 700, xlab = "Samples")
+        
+        factor.cmap$colors <- jColors$color
+        factor.cmap$breaks <- jColors$factors
+        factor.cmap$include.lowest <- TRUE
+        
+        population.cmap$colors <- levels(population)
+        population.cmap$breaks <- c("Group1","Group2","")
+        population.cmap$include.lowest <- TRUE
+        
+        par(mar = c(6.5,6,4,3)+0.1)  # make space for color keys
+        dendromat(dendo, matrix, height = 0.3, ylab = 'Distance')
+        
+        vkey(factor.cmap, 'Factors', y = 0.9, stretch = 3 )
+        vkey(population.cmap, 'Groups', y = 0.6, stretch = 3)
+        vkey(outliers.cmap, 'Dissimilarity', y = 0.0, stretch =2)
+        
+        dev.off()
+}
+
+
 # Apply Bonferroni cut-off as the default thresold value
 volcanoplot <- function(toptable, fold.change, t = 0.05 / length(gene.names), path){
 
     # Highlight genes that have an logFC greater than fold change
     # a p-value less than Bonferroni cut-off
-    toptable$threshold <- as.factor(abs(toptable$logFC)  > fold.change &
+    toptable$Significant <- as.factor(abs(toptable$logFC)  > fold.change &
                                         toptable$P.Value < t)
 
     # Construct the plot object
-    vol <- ggplot(data = toptable, aes(x = toptable$logFC, y = -log10(toptable$P.Value), colour = threshold)) +
+    vol <- ggplot(data = toptable, aes(x = toptable$logFC, y = -log10(toptable$P.Value), colour = Significant)) +
         geom_point(alpha = 0.4, size = 1.75)  + xlim(c(-max(toptable$logFC) - 0.1, max(toptable$logFC) + 0.1)) + ylim(c(0, max(-log10(toptable$P.Value)) + 0.5)) + xlab("log2 fold change") + ylab("-log10 p-value")
 
     # File saving
     filename <- paste(path, "volcano.png", sep = "")
     ggsave(filename, plot = vol, height = 6, width = 6)
 }
-
 
 get.volcanodata <- function(toptable){
 
@@ -371,7 +446,6 @@ get.pcplotdata <- function(Xpca, populations){
                       function(y) split(Xscores[, y], populations))
     names(Xscores) <- cols
 
-
    return(unlist(Xscores, recursive = FALSE))
 }
 
@@ -393,13 +467,13 @@ json.list <- append(json.list, list(tops = temp.toptable))
 X.toptable <- X[as.numeric(rownames(toptable)), ]
 
 # save toptable expression data
-if (! is.na(argv$expsavepath)){
-    save(X.toptable, expression.info, file = argv$expsavepath )
-}
+filename <- paste(run.dir,"expressionprofile.rData", sep = "")
+save(X.toptable, expression.info, file = filename)
+
 
 if ("Boxplot" %in% analysis.list){
     samples.boxplot(data, c(pop.colour1, pop.colour2),
-        c(pop.name1, pop.name2), path = output.dir)
+        c(pop.name1, pop.name2), path = run.dir) #runDir
 }
 
 if ("Volcano" %in% analysis.list){
@@ -408,7 +482,7 @@ if ("Volcano" %in% analysis.list){
     toptable.all <- find.toptable(X, newpclass,
         toptable.sortby, length(gene.names))
 
-    volcanoplot(toptable.all, fold.change, threshold.value, output.dir)
+    volcanoplot(toptable.all, fold.change, threshold.value, run.dir)
 
     # save volcanoplot top data as JSON
     volcanoplot.data <- get.volcanodata(toptable)
@@ -432,10 +506,14 @@ if ("PCA" %in% analysis.list){
 
 if ("Heatmap" %in% analysis.list){
     heatmap(X.toptable, expression.info, heatmap.rows = heatmap.rows,
-            dendrow, dendcol, dist.method, clust.method, output.dir)
+            dendrow, dendcol, dist.method, clust.method, run.dir)
+}
+
+if ("Clustering" %in% analysis.list){
+    clustering(X, dist.method, clust.method, expression.info)
 }
 
 if (length(json.list) != 0){
-    filename <- paste(output.dir, "data.json", sep = "")
+    filename <- paste(run.dir, "data.json", sep = "")
     write(toJSON(json.list, digits=I(4)), filename )
 }
---
+++ RCore/expression.R
@@ -0,0 +1,53 @@
+#!/usr/bin/Rsript
+# ---------------------------------------------------------------------------
+# Filename      : DGEA.R
+# Authors       : IsmailM, Nazrath, Suresh, Marian, Anissa
+# Description   : Retrieve individual gene expressions and convert to JSON
+# Run           : Rscript Expressions.R  --rundir ~/Desktop/ --geneid LOC100288410
+# ---------------------------------------------------------------------------
+
+library('argparser')    # Argument passing
+library('jsonlite')     # Convert R object to JSON format
+
+#############################################################################
+#                        Command Line Arguments                             #
+#############################################################################
+
+# set parsers for all input arguments
+parser <- arg_parser("This parser contains the input arguments")
+
+# General Parameters
+parser <- add_argument(parser, "--rundir", help="Full file path of rData file") 
+parser <- add_argument(parser, "--geneid", help="Row Id of the X matrix")    
+
+# allow arguments to be run via the command line
+argv   <- parse_args(parser)
+
+#############################################################################
+#                          Loading Saved Dataset                            #
+#############################################################################
+
+filename <- paste(argv$rundir,"expressionprofile.rData", sep = "")
+
+if (file.exists(filename)){
+    load(file = filename)
+}else{
+    print("ERROR:File not found")
+    q(save = "no")
+}
+
+#############################################################################
+#                          Retrieve Expression data                         #
+#############################################################################
+
+if((!is.na(argv$rundir))&&(!is.na(X.toptable))){
+    index.group1 <- which((expression.info['population']== 'Group1')==TRUE)
+    g1 <- list( x = names(X.toptable[argv$geneid, index.group1]),
+                y = as.double(X.toptable[argv$geneid, index.group1]))
+  
+    index.group2 <- which((expression.info['population']== 'Group2')==TRUE)
+    g2 <- list(x = names(X.toptable[argv$geneid, index.group2]),
+                   y = as.double(X.toptable[argv$geneid, index.group2]))
+    filename <- paste(argv$rundir,argv$geneid,".json", sep = "")
+    write(toJSON(list(list(group1 = g1 ,group2 = g2))), filename)
+}
